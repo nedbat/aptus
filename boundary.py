9ad08061bff48a94e1e42bcbeabf5cbdbf6346bf
nedbat <nedbat@localhost>
2007-09-12 01:44:25 +0000

    Command-line options.

--- boundary.py
+++ boundary.py
@@ -17,15 +17,8 @@ def trace_boundary(count_fn, w, h, maxiter, threshold=10000, progress_fn=None):
     counts = numpy.zeros((h, w), dtype=numpy.uint32)
     status = numpy.zeros((h, w), dtype=numpy.uint8)
     num_trace = 0
-    
-    def pix_count(x, y):
-        if status[y,x] == 0:
-            c = count_fn(x, -y)
-            counts[y,x] = c
-            status[y,x] = 1
-        else:
-            c = counts[y,x]
-        return c
+    num_pixels = 0
+    total_pixels = h * w
     
     for yi in xrange(h):
         for xi in xrange(w):
@@ -33,6 +26,7 @@ def trace_boundary(count_fn, w, h, maxiter, threshold=10000, progress_fn=None):
             if s == 0:
                 c = count_fn(xi, -yi)
                 counts[yi,xi] = c
+                num_pixels += 1
                 status[yi,xi] = s = 1
             else:
                 c = counts[yi,xi]
@@ -40,7 +34,6 @@ def trace_boundary(count_fn, w, h, maxiter, threshold=10000, progress_fn=None):
             comp_c = c or maxiter
             if s == 1 and comp_c >= threshold:
                 # Start a boundary trace.
-                num_trace += 1
                 status[yi,xi] == 2
                 curdir = DOWN
                 curx, cury = xi, yi
@@ -79,8 +72,14 @@ def trace_boundary(count_fn, w, h, maxiter, threshold=10000, progress_fn=None):
                         curx += 1
                     
                     # Get the count of the next position
-                    c2 = pix_count(curx, cury)
-                        
+                    if status[cury,curx] == 0:
+                        c2 = count_fn(curx, -cury)
+                        counts[cury,curx] = c2
+                        num_pixels += 1
+                        status[cury,curx] = 1
+                    else:
+                        c2 = counts[cury,curx]
+    
                     # If the same color, turn right, else turn left.
                     if c2 == c:
                         status[cury,curx] = 2
@@ -92,30 +91,37 @@ def trace_boundary(count_fn, w, h, maxiter, threshold=10000, progress_fn=None):
                         curdir = turn_left[curdir]
                     
                     start = False
-                    
-                # Now flood fill the region.  The points list has all the boundary
-                # points, so we only need to fill left and right from each of those.
-                for ptx, pty in points:
-                    curx = ptx
-                    while True:
-                        curx -= 1
-                        if curx < 0:
-                            break
-                        if status[pty,curx] != 0:
-                            break
-                        counts[pty,curx] = c
-                        status[pty,curx] = 2
-                    curx = ptx
-                    while True:
-                        curx += 1
-                        if curx > w-1:
-                            break
-                        if status[pty,curx] != 0:
-                            break
-                        counts[pty,curx] = c
-                        status[pty,curx] = 2
+
+                if points:
+                    num_trace += 1
     
-        progress_fn(float(yi+1)/h)
+                    # Now flood fill the region.  The points list has all the boundary
+                    # points, so we only need to fill left and right from each of those.
+                    for ptx, pty in points:
+                        curx = ptx
+                        while True:
+                            curx -= 1
+                            if curx < 0:
+                                break
+                            if status[pty,curx] != 0:
+                                break
+                            counts[pty,curx] = c
+                            num_pixels += 1
+                            status[pty,curx] = 2
+                        curx = ptx
+                        while True:
+                            curx += 1
+                            if curx > w-1:
+                                break
+                            if status[pty,curx] != 0:
+                                break
+                            counts[pty,curx] = c
+                            num_pixels += 1
+                            status[pty,curx] = 2
+                    
+                    progress_fn(float(num_pixels)/total_pixels)
+    
+        progress_fn(float(num_pixels)/total_pixels)
     
     print "Traced %s boundaries" % num_trace
     return counts
---
+++ options.py
@@ -0,0 +1,68 @@
+""" Options handling for Mand.
+"""
+
+import optparse, sys
+
+class MandOptions:
+    def __init__(self):
+        self.center = -0.5, 0.0
+        self.diam = 3.0, 3.0
+        self.size = 600, 600
+        self.maxiter = 999
+        self.trace = False
+    
+    def read_args(self, argv):
+        parser = optparse.OptionParser()
+        parser.add_option("-i", "--maxiter", dest="maxiter", help="set the maximum iteration count")
+        parser.add_option("-s", "--size", dest="size", help="set the pixel size of the image", metavar="WIDxHGT")
+        parser.add_option("-t", "--trace", dest="trace", action="store_true", help="use boundary tracing")
+        
+        options, args = parser.parse_args(argv)
+
+        if len(args) > 0:
+            xaos = XaosState()
+            xaos.read(sys.argv[1])
+            self.center = xaos.center
+            self.diam = xaos.diam
+            self.maxiter = xaos.maxiter
+
+        if options.maxiter:
+            self.maxiter = int(options.maxiter)
+        if options.size:
+            self.size = map(int, options.size.split('x'))
+        if options.trace:
+            self.trace = True
+            
+class XaosState:
+    """ The state of a Xaos rendering.
+    """
+    def __init__(self):
+        self.maxiter = 170
+        self.center = -0.75, 0.0
+        self.diam = 2.55, 2.55
+        
+    def read(self, f):
+        if isinstance(f, basestring):
+            f = open(f)
+        for l in f:
+            if l.startswith('('):
+                argv = l[1:-2].split()
+                if hasattr(self, 'handle_'+argv[0]):
+                    getattr(self, 'handle_'+argv[0])(*argv)
+                    
+    def handle_maxiter(self, op, maxiter):
+        self.maxiter = int(maxiter)
+        
+    def handle_view(self, op, cx, cy, rx, ry):
+        self.center = self.read_float(cx), self.read_float(cy)
+        self.diam = self.read_float(rx), self.read_float(ry)
+        
+    def read_float(self, fstr):
+        # Xaos writes out floats with extra characters tacked on the end sometimes.
+        # Very ad-hoc: try converting to float, and if it fails, chop off trailing
+        # chars until it works.
+        while True:
+            try:
+                return float(fstr)
+            except:
+                fstr = fstr[:-1]
