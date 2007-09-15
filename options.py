5e48262603c89897fdc8443b6d068e22928957d8
nedbat <nedbat@localhost>
2007-09-15 11:45:56 +0000

    Read the palette shift from Xaos files.

--- options.py
+++ options.py
@@ -10,10 +10,12 @@ class MandOptions:
         self.size = 600, 600
         self.maxiter = 999
         self.trace = False
-    
+        self.palette_phase = 0
+        
     def read_args(self, argv):
         parser = optparse.OptionParser()
         parser.add_option("-i", "--maxiter", dest="maxiter", help="set the maximum iteration count")
+        parser.add_option("--phase", dest="palette_phase", help="set the palette phase", metavar="PHASE")
         parser.add_option("-s", "--size", dest="size", help="set the pixel size of the image", metavar="WIDxHGT")
         parser.add_option("-t", "--trace", dest="trace", action="store_true", help="use boundary tracing")
         
@@ -25,9 +27,12 @@ class MandOptions:
             self.center = xaos.center
             self.diam = xaos.diam
             self.maxiter = xaos.maxiter
-
+            self.palette_phase = xaos.palette_phase
+            
         if options.maxiter:
             self.maxiter = int(options.maxiter)
+        if options.palette_phase:
+            self.palette_phase = int(options.palette_phase)
         if options.size:
             self.size = map(int, options.size.split('x'))
         if options.trace:
@@ -40,6 +45,7 @@ class XaosState:
         self.maxiter = 170
         self.center = -0.75, 0.0
         self.diam = 2.55, 2.55
+        self.palette_phase = 0
         
     def read(self, f):
         if isinstance(f, basestring):
@@ -56,6 +62,9 @@ class XaosState:
     def handle_view(self, op, cx, cy, rx, ry):
         self.center = self.read_float(cx), self.read_float(cy)
         self.diam = self.read_float(rx), self.read_float(ry)
+        
+    def handle_shiftpalette(self, op, phase):
+        self.palette_phase = int(phase)
         
     def read_float(self, fstr):
         # Xaos writes out floats with extra characters tacked on the end sometimes.
