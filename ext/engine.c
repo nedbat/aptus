7137013d01e9c593cbbf48c8f6d9a7032378bff1
Ned Batchelder <ned@nedbatchelder.com>
2007-09-24 01:40:36 +0000

    Experimenting with finding the slowdown with the mandelbrot_array method.

--- ext/engine.c
+++ ext/engine.c
@@ -160,6 +160,58 @@ mandelbrot_array(PyObject *self, PyObject *args)
     return Py_BuildValue("");
 }
 
+static int
+foo_count(int xi, int yi)
+{
+    aptfloat f = 1.0;
+    
+    int i;
+    for (i = 0; i < max_iter; i++) {
+        f *= 1.0000001;
+    }
+    
+    return 27;
+}
+
+static PyObject *
+foo_point(PyObject *self, PyObject *args)
+{
+    int xi, yi;
+    
+    if (!PyArg_ParseTuple(args, "ii", &xi, &yi)) {
+        return NULL;
+    }
+
+    int count = foo_count(xi, yi);
+
+    return Py_BuildValue("i", count);
+}
+    
+static PyObject *
+foo_array(PyObject *self, PyObject *args)
+{
+    PyArrayObject *arr;
+    
+    if (!PyArg_ParseTuple(args, "O!", &PyArray_Type, &arr)) {
+        return NULL;
+    }
+    
+    if (arr == NULL) {
+        return NULL;
+    }
+
+    int w = PyArray_DIM(arr, 1);
+    int h = PyArray_DIM(arr, 0);
+    int xi, yi;
+    for (yi = 0; yi < h; yi++) {
+        for (xi = 0; xi < w; xi++) {
+            *(npy_uint32 *)PyArray_GETPTR2(arr, yi, xi) = foo_count(xi, -yi);
+        }
+    }
+    
+    return Py_BuildValue("");
+}
+
 static PyObject *
 set_params(PyObject *self, PyObject *args)
 {
@@ -230,6 +282,8 @@ mandext_methods[] = {
     {"float_sizes", float_sizes, METH_VARARGS, "Get sizes of float types"},
     {"clear_stats", clear_stats, METH_VARARGS, "Clear the statistic counters"},
     {"get_stats", get_stats, METH_VARARGS, "Get the statistics as a dictionary"},
+    {"foo_point", foo_point, METH_VARARGS, ""},
+    {"foo_array", foo_array, METH_VARARGS, ""},
     {NULL, NULL}
 };
 
