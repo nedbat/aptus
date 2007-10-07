69c44ea0101a9955461a3f236c7e44b6408bcd66
nedbat <nedbat@localhost>
2007-10-07 15:30:35 +0000

    Cmdline needs option refactoring too.

--- src/cmdline.py
+++ src/cmdline.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from aptus.app import ConsoleProgressReporter, AptusApp, AptusMandelbrot
+from aptus.app import ConsoleProgressReporter, AptusApp
 from aptus.importer import importer
 from aptus.options import AptusOptions
 from aptus.palettes import all_palettes
@@ -13,24 +13,17 @@ class AptusCmdApp(AptusApp):
     def main(self, args):
         """ The main for the Aptus command-line tool.
         """
-        opts = AptusOptions()
+        opts = AptusOptions(self)
         opts.read_args(args)
-        
-        self.center = opts.center
-        self.diam = opts.diam
-        self.size = opts.size
-        self.iter_limit = opts.iter_limit
-        self.palette = opts.palette or all_palettes[0]
-        self.palette_phase = opts.palette_phase
-        self.supersample = opts.supersample
         
         m = self.create_mandel()
         
         m.progress = ConsoleProgressReporter()
         m.compute_pixels()
-        pix = m.color_pixels(self.palette, opts.palette_phase)
+        pix = m.color_pixels(self.palette, self.palette_phase)
         im = Image.fromarray(pix)
         if self.supersample > 1:
+            print "Resampling image..."
             im = im.resize(self.size, Image.ANTIALIAS)
         self.write_image(im, "foo.png")
 
