a1260349f013856001ef1f640d1c1e0fd6422e90
Ned Batchelder <ned@nedbatchelder.com>
2008-03-03 13:24:08 +0000

    TinyJson is separate now, supports all we need, and stats are written as real JSON.

--- src/tinyjson.py
+++ src/tinyjson.py
@@ -1,8 +1,11 @@
+# A teeny tiny JSON module.
+# Ned Batchelder, http://nedbatchelder.com
+
 # Safe eval, from http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/364469
 
 import compiler
 
-class Unsafe_Source_Error(Exception):
+class UnsafeSourceError(Exception):
     def __init__(self, error, descr=None, node=None):
         self.error = error
         self.descr = descr
@@ -46,7 +49,7 @@ class SafeEval(object):
         name = node.name.lower()
         if name in names:
             return names[name]
-        raise Unsafe_Source_Error("Unknown name", node.name, node)
+        raise UnsafeSourceError("Unknown name", node.name, node)
     
 class SafeEvalWithErrors(SafeEval):
 
@@ -65,5 +68,42 @@ def safe_eval(source, fail_on_error=True):
         raise
     try:
         return walker.visit(ast)
-    except Unsafe_Source_Error, err:
+    except UnsafeSourceError, err:
         raise
+
+
+class JsonWriter:
+    def dumps(self, v):
+        """ Return the JSON-stringified version of the value `v`.
+        """
+        if isinstance(v, (bool, int, float)):
+            return repr(v).lower()
+        elif isinstance(v, long):
+            return repr(v)[:-1] # drop the L suffix
+        elif isinstance(v, (list, tuple)):
+            return "[" + ",".join([ self.dumps(e) for e in v ]) + "]"
+        elif isinstance(v, str):
+            return '"' + v.replace('"', '\\"') + '"'
+        elif isinstance(v, dict):
+            return self.dumps_dict(v)
+        else:
+            raise Exception("Don't know how to serialize: %r" % v)
+
+    def dumps_dict(self, v, comma=",", colon=":", first_keys=[]):
+        """ Dump a dictionary to a JSON string.
+            `comma` and `colon` override the the separators in the dictionary.
+            `first_keys` is a list of keys to write first.
+        """
+        keys = v.keys()
+        keys = [ fk for fk in first_keys if fk in keys ] + [ k for k in keys if k not in first_keys ]
+        return "{" + comma.join([ self.dumps(k) + colon + self.dumps(v[k]) for k in keys ]) + "}"
+
+class JsonReader:
+    def loads(self, s):
+        return safe_eval(s)
+
+def loads(s):
+    return safe_eval(s)
+
+def dumps(v):
+    return JsonWriter().dumps(v)
