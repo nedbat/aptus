d13475e5a9f78707cb1d117f9cc4e3f0bf15d909
nedbat <nedbat@localhost>
2008-06-17 12:27:04 +0000

    Refactored help into its own file.

--- src/gui/__init__.py
+++ src/gui/__init__.py
@@ -3,7 +3,7 @@
     Copyright 2007-2008, Ned Batchelder
 """
 
-from aptus import data_file, __version__
+from aptus import data_file
 from aptus.compute import AptusCompute
 from aptus.progress import ConsoleProgressReporter, IntervalProgressReporter, AggregateProgressReporter
 from aptus.importer import importer
@@ -15,15 +15,10 @@ wx = importer('wx')
 numpy = importer('numpy')
 Image = importer('Image')
 
-import math, os, os.path, sys, webbrowser
-import wx.lib.layoutf
-import wx.html 
+import math, os, os.path, sys
 import wx.lib.newevent
 from wx.lib.evtmgr import eventManager
 from wx.lib.scrolledpanel import ScrolledPanel
-
-# There are a few places we conditionalize on platform.
-is_mac = ('wxMac' in wx.PlatformInfo)
 
 # A pre-set list of places to visit, with the j command.
 jumps = [
@@ -745,93 +740,13 @@ class AptusMainFrame(wx.Frame):
                 self.message("Don't understand how to write file '%s'" % pth)
                 
     def cmd_help(self, event_unused):
-        dlg = HtmlDialog(self, help_html, "Aptus")
+        # help is a built-in.  pylint: disable-msg=W0622     
+        from aptus.gui import help
+        dlg = help.HelpDlg(self)
         dlg.ShowModal()
 
     def cmd_show_youarehere(self, event_unused):
         YouAreHereFrame(self.panel).Show()
-
-
-class HtmlDialog(wx.Dialog):
-    def __init__(self, parent, html_text, caption,
-                 pos=wx.DefaultPosition, size=(500,530),
-                 style=wx.DEFAULT_DIALOG_STYLE):
-        wx.Dialog.__init__(self, parent, -1, caption, pos, size, style)
-        x, y = pos
-        if x == -1 and y == -1:
-            self.CenterOnScreen(wx.BOTH)
-
-        self.html = wx.html.HtmlWindow(self, -1)
-        self.html.Bind(wx.html.EVT_HTML_LINK_CLICKED, self.on_link_clicked)
-        ok = wx.Button(self, wx.ID_OK, "OK")
-        ok.SetDefault()
-        
-        lc = wx.lib.layoutf.Layoutf('t=t#1;b=t5#2;l=l#1;r=r#1', (self,ok))
-        self.html.SetConstraints(lc)
-        self.html.SetPage(html_text)
-        
-        lc = wx.lib.layoutf.Layoutf('b=b5#1;r=r5#1;w!80;h*', (self,))
-        ok.SetConstraints(lc)
-        
-        self.SetAutoLayout(1)
-        self.Layout()
-
-    def on_link_clicked(self, event):
-        url = event.GetLinkInfo().GetHref()
-        webbrowser.open(url)
-
-        
-# The help text
-
-terms = {
-    'ctrl': 'cmd' if is_mac else 'ctrl',
-    'iconsrc': data_file('icon48.png'),
-    'version': __version__,
-    }
-    
-help_html = """\
-<table width='100%%'>
-<tr>
-    <td width='50' valign='top'><img src='%(iconsrc)s'/></td>
-    <td valign='top'>
-        <b>Aptus %(version)s</b>, Mandelbrot set explorer.<br>
-        Copyright 2007-2008, Ned Batchelder.<br>
-        <a href='http://nedbatchelder.com/code/aptus'>http://nedbatchelder.com/code/aptus</a>
-    </td>
-</tr>
-</table>
-
-<p><b>Controls:</b></p>
-
-<blockquote>
-<b>a</b>: set the angle of rotation.<br>
-<b>b</b>: set the radius of the bailout circle.<br>
-<b>c</b>: toggle continuous coloring.<br>
-<b>h</b> or <b>?</b>: show this help.<br>
-<b>i</b>: set the limit on iterations.<br>
-<b>j</b>: jump among a few pre-determined locations.<br>
-<b>n</b>: open a new window.<br>
-<b>r</b>: redraw the current image.<br>
-<b>s</b>: save the current image or settings.<br>
-<b>&lt;</b> or <b>&gt;</b>: switch to the next palette.<br>
-<b>,</b> or <b>.</b>: cycle the current palette one color.<br>
-<b>;</b> or <b>'</b>: stretch the palette colors (+%(ctrl)s: just a little), if continuous.<br>
-<b>[</b> or <b>]</b>: adjust the hue of the palette (+%(ctrl)s: just a little).<br>
-<b>{</b> or <b>}</b>: adjust the saturation of the palette (+%(ctrl)s: just a little).<br>
-<b>0</b> (zero): reset all palette adjustments.<br>
-<b>space</b>: drag mode: click to drag the image to a new position.<br>
-<b>left-click</b>: zoom in (+%(ctrl)s: just a little).<br>
-<b>right-click</b>: zoom out (+%(ctrl)s: just a little).<br>
-<b>left-drag</b>: select a new rectangle to display.<br>
-<b>middle-drag</b>: drag the image to a new position.
-</blockquote>
-
-<p>Built with
-<a href='http://python.org'>Python</a>, <a href='http://wxpython.org'>wxPython</a>,
-<a href='http://numpy.scipy.org/'>numpy</a>, and
-<a href='http://www.pythonware.com/library/pil/handbook/index.htm'>PIL</a>.
-Thanks to Rob McMullen and Paul Ollis for help with the wxPython drawing code.</p>
-""" % terms
 
 
 class AptusGuiApp(wx.PySimpleApp):
