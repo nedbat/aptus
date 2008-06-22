fbbffdd98de60423df7b11bfd176fa21780412b1
nedbat <nedbat@localhost>
2008-06-22 00:39:17 +0000

    More turbulence about palettes panel, but it still doesn't resize properly...

--- src/gui/palettespanel.py
+++ src/gui/palettespanel.py
@@ -5,48 +5,54 @@ from aptus.importer import importer
 wx = importer("wx")
 from wx.lib.scrolledpanel import ScrolledPanel
 
-class PalettesPanel(wx.Panel):
-    def __init__(self, parent, palettes, size=wx.DefaultSize):
-        wx.Panel.__init__(self, parent, size=size)
-        
-        self.palettes = palettes
-        
-        scrolledpanel = ScrolledPanel(self, -1, #size=(140, 300),
-                    style = wx.TAB_TRAVERSAL|wx.SUNKEN_BORDER)
+class PaletteWin(wx.Window):
+    def __init__(self, parent, palette, size=wx.DefaultSize):
+        wx.Window.__init__(self, parent, size=size)
+        self.palette = palette
 
-        self.panel = wx.Panel(scrolledpanel, size=(200,300))
-        self.panel.Bind(wx.EVT_PAINT, self.on_paint)
-        self.panel.Bind(wx.EVT_SIZE, self.on_size)
-
-        self.stripe_height = 50
-
-        #box = wx.BoxSizer(wx.VERTICAL)
-        #box.Add(self.panel)
-        #scrolledpanel.SetSizer(box)
-        scrolledpanel.SetAutoLayout(True)
-        scrolledpanel.SetupScrolling()
+        self.Bind(wx.EVT_PAINT, self.on_paint)
         
     def on_paint(self, event_unused):
-        dc = wx.PaintDC(self.panel)
-        cw_unused, ch = self.GetClientSize()
-        for y, pal in enumerate(self.palettes):
-            self.paint_palette(dc, pal, y*self.stripe_height, self.stripe_height)
-            
-    def paint_palette(self, dc, pal, y0, height):
-        cw, ch_unused = self.GetClientSize()
-        ncolors = len(pal.colors)
+        dc = wx.PaintDC(self)
+        cw, ch = self.GetClientSize()
+        ncolors = len(self.palette.colors)
         width = float(cw)/ncolors
         for c in range(0, ncolors):
-            dc.SetPen(wx.Pen(wx.Colour(*pal.colors[c]), 1))
-            dc.SetBrush(wx.Brush(wx.Colour(*pal.colors[c]), wx.SOLID))
-            dc.DrawRectangle(int(c*width), y0, int(width+1), height)
-    
+            dc.SetPen(wx.Pen(wx.Colour(*self.palette.colors[c]), 1))
+            dc.SetBrush(wx.Brush(wx.Colour(*self.palette.colors[c]), wx.SOLID))
+            dc.DrawRectangle(int(c*width), 0, int(width+1), ch)
+        
+class PalettesPanel(ScrolledPanel):
+    def __init__(self, parent, palettes, size=wx.DefaultSize):
+        ScrolledPanel.__init__(self, parent, size=size)
+        
+        self.palettes = palettes
+        self.pal_height = 30
+        
+        self.sizer = wx.FlexGridSizer(len(self.palettes), 1)
+        for pal in self.palettes:
+            self.a_palette = PaletteWin(self, pal, size=(200, self.pal_height))
+            self.sizer.Add(self.a_palette, wx.EXPAND)
+
+        self.sizer.AddGrowableCol(0)
+        self.sizer.SetFlexibleDirection(wx.HORIZONTAL)
+        self.SetSizer(self.sizer)
+        self.SetAutoLayout(True)
+        self.SetupScrolling()
+        #self.Bind(wx.EVT_SIZE, self.on_size)
+        
     def on_size(self, event_unused):
-        self.Refresh()
+        h = len(self.palettes) * self.pal_height
+        cw, _ = self.GetClientSize()
+        self.SetVirtualSize((cw, h))
+        self.sizer.SetMinSize((cw, h))
+        print "Laying out: %r" % self.sizer.GetFlexibleDirection()
+        self.Layout()
+        self.sizer.Layout()
 
 
 class PalettesFrame(wx.MiniFrame):
     def __init__(self, palettes):
         wx.MiniFrame.__init__(self, None, title='Palettes', size=(250, 350),
-            style=wx.DEFAULT_MINIFRAME_STYLE|wx.CLOSE_BOX)
+            style=wx.DEFAULT_FRAME_STYLE)
         self.panel = PalettesPanel(self, palettes)
