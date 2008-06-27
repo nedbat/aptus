58e06775baebea1cda787406421b16bd27cd7086
nedbat <nedbat@localhost>
2008-06-27 02:20:15 +0000

    Quiet a pylint warning.

--- src/gui/misc.py
+++ src/gui/misc.py
@@ -8,7 +8,7 @@ class AptusToolFrame(wx.MiniFrame):
     """ A frame for tool windows.
     """
     # This handles getting the styles right for miniframes.
-    def __init__(self, parent, title='', size=wx.DefaultSize):
+    def __init__(self, parent_unused, title='', size=wx.DefaultSize):
         # If I pass parent into MiniFrame, the focus gets messed up, and keys don't work anymore!?
         wx.MiniFrame.__init__(self, None, title=title, size=size,
             style=wx.DEFAULT_FRAME_STYLE
