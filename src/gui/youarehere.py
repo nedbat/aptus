98fe5df3a10da0b670fe97171e01b699f9312d7c
nedbat <nedbat@localhost>
2008-06-27 02:19:11 +0000

    YouAreHere now has a draggable rectangle, with cursors and everything!

--- src/gui/youarehere.py
+++ src/gui/youarehere.py
@@ -25,7 +25,9 @@ class YouAreHerePanel(ComputePanel):
         self.Bind(wx.EVT_WINDOW_DESTROY, self.on_destroy)
         self.Bind(wx.EVT_SIZE, self.on_size)
         self.Bind(wx.EVT_IDLE, self.on_idle)
+        self.Bind(wx.EVT_LEFT_DOWN, self.on_left_down)
         self.Bind(wx.EVT_LEFT_UP, self.on_left_up)
+        self.Bind(wx.EVT_MOTION, self.on_motion)
         
         eventManager.Register(self.on_coloring_changed, EVT_APTUS_COLORING_CHANGED, self.mainwin)
         eventManager.Register(self.on_computation_changed, EVT_APTUS_COMPUTATION_CHANGED, self.mainwin)
@@ -36,6 +38,9 @@ class YouAreHerePanel(ComputePanel):
         self.on_computation_changed(None)
         self.on_geometry_changed(None)
         
+        self.dragging = False
+        self.drag_pt = None
+
     def on_destroy(self, event_unused):
         eventManager.DeregisterListener(self.on_coloring_changed)
         eventManager.DeregisterListener(self.on_computation_changed)
@@ -53,10 +58,41 @@ class YouAreHerePanel(ComputePanel):
         if not self.hererect:
             self.calc_rectangle()
 
+    def on_left_down(self, event):
+        mouse_pt = event.GetPosition()
+        if self.hererect.Contains(mouse_pt):
+            self.dragging = True
+            self.drag_pt = mouse_pt
+
     def on_left_up(self, event):
-        mx, my = event.GetPosition()
+        # Reposition the main window.
+        if self.dragging:
+            # Dragging the rect: recenter on its center.
+            mx = self.hererect.x + self.hererect.width/2
+            my = self.hererect.y + self.hererect.height/2
+        else:
+            # Clicking outside the rect: recenter there.
+            mx, my = event.GetPosition()
+
         self.mainwin.recenter(self.m.coords_from_pixel(mx, my))
-        
+        self.dragging = False
+
+    def on_motion(self, event):
+        self.set_cursor(event)
+        if self.dragging:
+            mouse_pt = event.GetPosition()
+            self.hererect.Offset((mouse_pt.x - self.drag_pt.x, mouse_pt.y - self.drag_pt.y))
+            self.drag_pt = mouse_pt
+            self.Refresh()
+
+    def set_cursor(self, event):
+        # Set the proper cursor:
+        mouse_pt = event.GetPosition()
+        if self.dragging or self.hererect.Contains(mouse_pt):
+            self.SetCursor(wx.StockCursor(wx.CURSOR_SIZING))
+        else:
+            self.SetCursor(wx.StockCursor(wx.CURSOR_DEFAULT))
+
     def on_coloring_changed(self, event_unused):
         if self.m.copy_coloring(self.mainwin.m):
             self.coloring_changed()
@@ -89,7 +125,7 @@ class YouAreHerePanel(ComputePanel):
         if h < 3:
             h = 3
             uy -= 1
-        self.hererect = (ux, uy, w, h)
+        self.hererect = wx.Rect(ux, uy, w, h)
         self.Refresh()
         
     def on_paint_extras(self, dc):
