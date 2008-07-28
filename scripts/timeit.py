d6c15283ff80b889884d116a22b635c5127c3c9d
nedbat <nedbat@localhost>
2008-07-28 22:13:00 +0000

    Add another view to time to timeit.py

--- scripts/timeit.py
+++ scripts/timeit.py
@@ -1,14 +1,33 @@
 from aptus.compute import AptusCompute
 from aptus.progress import NullProgressReporter
 
-import time
+import sys, time
 
-def timeit():
+def timeit(args):
     m = AptusCompute()
     m.progress = NullProgressReporter()
 
     nruns = 100
     grandtotal = 0
+    
+    if not args:
+        case = 'a'
+    else:
+        case = args[0]
+        
+    if case == 'a':
+        m.center = -0.6, 0.0
+        m.diam = 3.0, 3.0
+        m.size = 600, 600
+    elif case == 'b':
+        m.center = -1.8605327670201655, -1.2705648690517021e-005
+        m.diam = 2.92062690996144e-010, 2.92062690996144e-010
+        m.size = 600, 600
+        m.iter_limit = 99999
+        nruns = 20
+    else:
+        print "huh?"
+        return
     
     for i in range(nruns):
         m.clear_results()
@@ -23,4 +42,4 @@ def timeit():
     
 
 if __name__ == '__main__':
-    timeit()
+    timeit(sys.argv[1:])
