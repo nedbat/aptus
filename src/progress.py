f173eff371bc4d4902693b7444a27b266c2aa3e6
Ned Batchelder <ned@nedbatchelder.com>
2010-01-30 11:55:02 -0500

    Progress reporting is now parallel-capable.

--- src/progress.py
+++ src/progress.py
@@ -13,10 +13,17 @@ class NullProgressReporter:
         """
         pass
     
-    def progress(self, frac_done, info=''):
-        """ Called repeatedly to report progress.  `frac_done` is a float between
-            zero and one indicating the fraction of work done.  `info` is a
-            string giving some information about what just got completed.
+    def progress(self, arg, num_done, info=''):
+        """ Called repeatedly to report progress.
+        
+        `arg` is an opaque argument, the caller can use it for whatever they want.
+        
+        `num_done` is a int indicating the count of progress.  There is no
+        defined range for `num_done`, it is assumed that the caller knows what
+        work is being done, and what the number mean.
+        
+        `info` is a string giving some information about what's been done.
+        
         """
         pass
     
@@ -38,10 +45,10 @@ class IntervalProgressReporter:
         self.latest = time.time()
         self.reporter.begin()
         
-    def progress(self, frac_done, info=''):
+    def progress(self, arg, num_done, info=''):
         now = time.time()
         if now - self.latest > self.nsec:
-            self.reporter.progress(frac_done, info)
+            self.reporter.progress(arg, num_done, info)
             self.latest = now
             
     def end(self):
@@ -61,13 +68,14 @@ class AggregateProgressReporter:
         for kid in self.kids:
             kid.begin()
             
-    def progress(self, frac_done, info=''):
+    def progress(self, arg, num_done, info=''):
         for kid in self.kids:
-            kid.progress(frac_done, info)
+            kid.progress(arg, num_done, info)
     
     def end(self):
         for kid in self.kids:
             kid.end()
+
 
 # Cheap way to measure and average a number of runs.
 nruns = 0
@@ -75,11 +83,16 @@ totaltotal = 0
 
 class ConsoleProgressReporter:
     """ A progress reporter that writes lines to the console.
+    
+    This `progress` function interprets the `num_done` arg as a fraction, in
+    millionths.
+    
     """
     def begin(self):
         self.start = time.time()
 
-    def progress(self, frac_done, info=''):
+    def progress(self, arg, num_done, info=''):
+        frac_done = num_done / 1000000.0
         now = time.time()
         so_far = int(now - self.start)
         to_go = int(so_far / frac_done * (1-frac_done))
@@ -88,7 +101,7 @@ class ConsoleProgressReporter:
         print "%5.2f%%: %11s done, %11s to go, eta %10s%s" % (
             frac_done*100, duration(so_far), duration(to_go), future(to_go), info
             )
-    
+
     def end(self):
         total = time.time() - self.start
         global totaltotal, nruns
